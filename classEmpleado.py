b67f8ac5c2ccaa4b7390ba0955ad8ead4aa34c38
CarinaGiovine <75708023+CarinaGiovine@users.noreply.github.com>
2021-05-01 20:25:23 -0300

    Update classEmpleado.py

--- classEmpleado.py
+++ classEmpleado.py
@@ -4,6 +4,14 @@ Created on Sat May  1 20:09:18 2021
 
 @author: CarinaGiovine
 """
+''' Crear una clase Empleado que modele la información que una empresa mantiene sobre cada empleado: código, sueldo base, pago por hora extra, 
+horas extras realizadas en el mes, casado o no y número de hijos.
+Al crear los objetos de esta clase se deberán proporcionar los datos de un empleado, y los métodos deberán permitir realizar:
+a)Cálculo de sueldo incluyendo pago de horas extras.
+b)Cálculo de retenciones. Suponer 5% si es casado y 5% por cada hijo.
+c)Cálculo del sueldo neto
+d)Mostrar el detalle de pago
+'''
 
 class Empleado():
     
